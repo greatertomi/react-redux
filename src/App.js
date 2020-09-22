69343bbf68572a9e22bdeb67779ef9c83ae9e309
Oshalusi John <oshalusijohn@gmail.com>
2020-09-22 23:59:33 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {selectSong} from './actions'
+import SongList from "./components/SongList";
 
 const App = () => {
   return (
     <div>
       App
+      <SongList />
     </div>
   );
 };
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 
 class SongList extends Component{
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div key={song.title}>
+          {song.title}
+          <button className="btn btn-primary">Select</button>
+        </div>
+      )
+    })
+  }
+
   render() {
-    return <div>SongList</div>
+    console.log(this.props);
+    return (
+      <div>
+        SongList
+        {this.renderList()}
+      </div>
+    )
   }
 }
 
-const mapStateToProps = (state) => {
-  console.log(state);
-
-  return state
+const mapStateToProps = state => {
+  return {songs: state.songs}
 };
 
 export default connect(mapStateToProps) (SongList);
